a56a4b862badd4b388667b15054aae927dc5922f
cimyjh <cimyjh@gmail.com>
2020-06-03 20:15:51 +0900

    Initial commit

---
+++ src/main/java/com/dmc/oauth2/web/IndexController.java
@@ -0,0 +1,40 @@
+package com.dmc.oauth2.web;
+
+import com.dmc.oauth2.config.auth.LoginUser;
+import com.dmc.oauth2.config.auth.dto.SessionUser;
+import com.dmc.oauth2.service.PostsService;
+import com.dmc.oauth2.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
---
+++ src/main/java/com/dmc/oauth2/web/PostsApiController.java
@@ -0,0 +1,44 @@
+package com.dmc.oauth2.web;
+
+import com.dmc.oauth2.service.PostsService;
+import com.dmc.oauth2.web.dto.PostsListResponseDto;
+import com.dmc.oauth2.web.dto.PostsResponseDto;
+import com.dmc.oauth2.web.dto.PostsSaveRequestDto;
+import com.dmc.oauth2.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+}
